2401a22cfc08aff8d9f08b12db8f11a9e0c33082
roman <roman.kalkreuth@rwth-aachen.de>
2026-05-07 08:54:13 +0200

    slurm files output added

--- results/slurm/sbatch-cgp-single-log.sh
+++ results/slurm/sbatch-cgp-single-log.sh
@@ -1,6 +1,6 @@
 for n in $(seq 1 30); do
         for i in $(seq 0 29); do
                 sleep 1
-                sbatch --time=1-00:00:00 --mem-per-cpu=3000 --job-name=tinyverse-and-cgp-log --wrap="python -m examples.analysis.max.test_max_plus_mul_cgp_single_log $d 2" --output=""
+                sbatch --time=1-00:00:00 --mem-per-cpu=3000 --job-name=tinyverse-and-cgp-log --wrap="python -m examples.analysis.max.test_max_plus_mul_cgp_single_log $d 2" --output="max-cgp-single-log-$d-$i.out"
         done
 done
--- results/slurm/sbatch-cgp-single.sh
+++ results/slurm/sbatch-cgp-single.sh
@@ -1,6 +1,6 @@
 for n in $(seq 1 10); do
         for i in $(seq 0 29); do
                 sleep 1
-                sbatch --time=1-00:00:00 --mem-per-cpu=3000 --job-name=tinyverse-max-cgp --wrap="python -m examples.analysis.max.test_max_plus_mul_cgp_single $d 1" --output=""
+                sbatch --time=1-00:00:00 --mem-per-cpu=3000 --job-name=tinyverse-max-cgp --wrap="python -m examples.analysis.max.test_max_plus_mul_cgp_single $d 1" --output="max-cgp-single-$d-$i.out"
         done
 done
